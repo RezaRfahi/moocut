61f6f05b45028b80e4520dadc9f17e5503896fd7
RezaRfahi <refahireza82@gmail.com>
2023-05-20 21:51:05 +0430

    modify salon migration

--- Backend/BarberBase/database/migrations/2023_05_20_144221_create_salons_table.php
+++ Backend/BarberBase/database/migrations/2023_05_20_144221_create_salons_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('salons', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('tel')->unique();
+            $table->string('postcode')->unique();
+            $table->date('establish');
+            $table->enum('status', ['open', 'closed'])->default('open');
+            $table->time('start')->default('08:00:00');
+            $table->time('finish')->default('23:00:00');
+            $table->point('map')->nullable()->index('map_index');
+            $table->text('breaks')->nullable();
             $table->timestamps();
         });
     }

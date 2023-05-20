1c95ffec59d947e6c8d572206a911230fcb69fc8
RezaRfahi <refahireza82@gmail.com>
2023-05-20 19:15:20 +0430

    create salon model with all

---
+++ Backend/BarberBase/app/Http/Controllers/SalonController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Salon;
+use App\Http\Requests\StoreSalonRequest;
+use App\Http\Requests\UpdateSalonRequest;
+
+class SalonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSalonRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Salon $salon)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Salon $salon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSalonRequest $request, Salon $salon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Salon $salon)
+    {
+        //
+    }
+}

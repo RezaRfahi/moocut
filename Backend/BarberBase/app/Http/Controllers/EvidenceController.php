013c6018f67c1f429652d425122311deedd44c48
RezaRfahi <refahireza82@gmail.com>
2023-05-26 15:46:12 +0430

    create Evidence model with all

---
+++ Backend/BarberBase/app/Http/Controllers/EvidenceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Evidence;
+use App\Http\Requests\StoreEvidenceRequest;
+use App\Http\Requests\UpdateEvidenceRequest;
+
+class EvidenceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEvidenceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Evidence $evidence)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Evidence $evidence)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEvidenceRequest $request, Evidence $evidence)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Evidence $evidence)
+    {
+        //
+    }
+}
